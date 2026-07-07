d9f174b2935fab84a56990812ba55bf395694f38
Ruth Rojas <ruro122020@gmail.com>
2026-07-07 08:48:54 -0400

    scaffold axum/tokio and stub HTTP handlers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use axum::{routing::{get, post}, Router};
+
+async fn health() -> &'static str {
+  "OK\n"
+}
+
+async fn turn_on() -> &'static str{
+  "led: on\n"
+}
+
+async fn turn_off() -> &'static str{
+  "led: off\n"
+}
+
 fn main() {
     println!("Hello, world!");
 }
